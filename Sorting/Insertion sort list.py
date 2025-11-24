618f5a8c995fdb487132e8e9785eba6c36ea3fd6
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-11-24 18:49:50 +0530

    Update Insertion sort list.py

--- Sorting/Insertion sort list.py
+++ Sorting/Insertion sort list.py
@@ -1,32 +1,15 @@
 class Solution:
-    def insertionSortList(self, head):
-        if not head:
-            return None
+    def insertionSort(self, nums):
+        n = len(nums)
 
-        dummy = ListNode(0)   # helps in insertion
-        curr = head
+        for i in range(1, n):
+            key = nums[i]
+            j = i - 1
 
-        while curr:
-            nxt = curr.next   # store next node
-            prev = dummy
+            while key < nums[j] and j >= 0:
+                nums[j+1] = nums[j]
+                j -= 1
 
-            # find position
-            while prev.next and prev.next.val < curr.val:
-                prev = prev.next
+            nums[j + 1] = key
 
-            # insert node
-            curr.next = prev.next
-            prev.next = curr
-
-            curr = nxt
-
-        return dummy.next
-
-arr = [4,2,1,3]   # Input
-print("Input:", arr)
-
-head = list_to_linkedlist(arr)               # list → linked list
-sorted_head = Solution().insertionSortList(head)  # sort
-result = linkedlist_to_list(sorted_head)     # linked list → list
-
-print("Output:", result)
+        return nums
