547c526289ce56904d98db69dbfde34696883d63
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-02-21 20:36:07 +0530

    Implement second method for string rotation
    Added a second method for rotating strings.

--- Strings/Rotate_String.py
+++ Strings/Rotate_String.py
@@ -1,7 +1,17 @@
-class Solution:    
+class Solution:
+    # Method 1 
     def rotateString(self, s, goal):
         tot_str = s + s
         if goal in tot_str:
             return True
         else:
             return False
+
+    # Method 2
+    def rotateString(self, s, goal):
+        for i in range(len(s)):
+            rotated = s[i:] + s[:i]
+            if rotated == goal:
+                return True
+        
+        return False
