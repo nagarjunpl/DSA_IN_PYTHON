407ff892d863ea4ff65489f6b552eb208d077343
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-02-25 19:51:20 +0530

    Update Rotate_String.py

--- Strings/Rotate_String.py
+++ Strings/Rotate_String.py
@@ -1,17 +1,27 @@
 class Solution:
-    # Method 1 
-    def rotateString(self, s, goal):
-        tot_str = s + s
-        if goal in tot_str:
-            return True
-        else:
+    # Method 1: Using s + s trick (Optimal)
+    def rotateString_method1(self, s, goal):
+        if len(s) != len(goal):
             return False
-
-    # Method 2
-    def rotateString(self, s, goal):
+        
+        tot_str = s + s
+        return goal in tot_str
+    
+    # Method 2: Brute force rotation
+    def rotateString_method2(self, s, goal):
+        if len(s) != len(goal):
+            return False
+        
         for i in range(len(s)):
             rotated = s[i:] + s[:i]
             if rotated == goal:
                 return True
         
         return False
+
+sol = Solution()
+
+s = "abcde"
+goal = "cdeab"
+print("Method 1 Result:", sol.rotateString_method1(s, goal))
+print("Method 2 Result:", sol.rotateString_method2(s, goal))
