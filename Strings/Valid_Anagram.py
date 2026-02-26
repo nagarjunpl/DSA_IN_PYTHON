094fa58e1db1ae65677f9a1bfd5e2d8e22dbc572
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-02-26 20:41:49 +0530

    Implement user input for anagram string check
    Added user input for string comparison and output result.

--- Strings/Valid_Anagram.py
+++ Strings/Valid_Anagram.py
@@ -10,3 +10,10 @@ class Solution:
                 s = s.replace(t[i], '', 1)   # it remove one occurrence in s
         
         return True
+        
+s = input("Enter first string: ")    #Enter first string: listen
+t = input("Enter second string: ")   #Enter second string: silent
+
+sol = Solution()
+result = sol.anagramStrings(s, t)
+print("Are the strings anagrams?", result) # Are the strings anagrams? True
