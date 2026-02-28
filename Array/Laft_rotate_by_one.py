3c6e43084591100334d77405b5869692f1a4db7f
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-02-28 20:14:26 +0530

    Add example usage for array rotation
    Added example usage for the rotateArrayByOne method.

--- Array/Laft_rotate_by_one.py
+++ Array/Laft_rotate_by_one.py
@@ -6,5 +6,12 @@ class Solution:
             nums[i] = nums[i + 1]
 
         nums[-1] = first
-
         return nums
+
+
+nums = [1, 2, 3, 4, 5]
+obj = Solution()
+result = obj.rotateArrayByOne(nums)
+
+print("Input:  [1, 2, 3, 4, 5]")
+print("Output:", result)
