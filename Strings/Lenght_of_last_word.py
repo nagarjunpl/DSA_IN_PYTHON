1afe4fd05d65629b0468f0e2648d263fcc4d2d7c
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-03-04 19:31:12 +0530

    Update Lenght_of_last_word.py

--- Strings/Lenght_of_last_word.py
+++ Strings/Lenght_of_last_word.py
@@ -1,6 +1,10 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        new = s.strip().split()   # removes spaces from ends, splits by spaces into list
-        rev = new[::-1]           # reverses the list
-        for j in rev[0:2]:        # takes the first word from the reversed list
-            return len(j)         # returns its length
+        new = s.strip().split()
+        rev = new[::-1]
+        for j in rev[0:2]:
+            return len(j)
+
+s = "Hello World"
+obj = Solution()
+print(obj.lengthOfLastWord(s)) # Output: 5
