72b7300113b6fa112c78407461816416192e73d7
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-03-09 21:21:40 +0530

    Update Two_Sum_Problem.py

--- Array/Two_Sum_Problem.py
+++ Array/Two_Sum_Problem.py
@@ -1,9 +1,13 @@
 class Solution:
     def twoSum(self, nums, target):
         seen = {}
-
         for i, num in enumerate(nums):
             complement = target - num
             if complement in seen:
                 return [seen[complement], i]
             seen[num] = i
+
+nums = [3,2,4]
+target = 6
+obj = Solution()
+print(obj.twoSum(nums, target)) # Output : [1,2]
