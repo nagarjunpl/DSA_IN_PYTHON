48ea71f47704aebfd9aaa03f90e887388d909917
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-03-10 17:46:36 +0530

    Update Sorted_Array_Checker.py

--- Array/Sorted_Array_Checker.py
+++ Array/Sorted_Array_Checker.py
@@ -1,8 +1,11 @@
 class Solution:
     def isSorted(self, nums):
-
         for i in range(len(nums) - 1):
             if nums[i] > nums[i + 1]:
                 return False
 
         return True
+
+nums = [1, 2, 3, 4, 5]
+obj = Solution()
+print(obj.isSorted(nums))
