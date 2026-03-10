651415aee486255c8962f43b542a7f024b02c5a2
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-03-10 17:59:35 +0530

    Update Single_Number-I.py

--- Array/Single_Number-I.py
+++ Array/Single_Number-I.py
@@ -1,9 +1,12 @@
 class Solution:
     def singleNumber(self, nums):
-
         single_number = 0
 
         for num in nums:
             single_number ^= num
             
         return single_number
+
+nums = [4,1,2,1,2]
+obj = Solution()
+print(obj.singleNumber(nums)) # Output : 4
