5845af8f9721ad41dbe0604125663c174d9c9d79
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-03-13 19:14:14 +0530

    Update Recursive_Bubble_sort.py

--- Sorting/Recursive_Bubble_sort.py
+++ Sorting/Recursive_Bubble_sort.py
@@ -7,8 +7,10 @@ class Solution:
             for i in range(n-1):
                 if nums[i] > nums[i+1]:
                     nums[i+1], nums[i] = nums[i], nums[i+1]
-
             sort_rec(nums, n-1)
-
         sort_rec(nums, len(nums))
         return nums
+
+nums = [5, 1, 4, 2, 8]
+obj = Solution()
+print(obj.bubbleSort(nums)) # Output: [1, 2, 4, 5, 8]
