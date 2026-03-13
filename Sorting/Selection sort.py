159d0f4fb7649d0dc5a38055cee98491f9045318
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-03-13 19:15:49 +0530

    Update Selection sort.py

--- Sorting/Selection sort.py
+++ Sorting/Selection sort.py
@@ -1,13 +1,15 @@
 class Solution:
     def selectionSort(self, nums):
         n = len(nums)
-        
         for i in range(n):
             min_index = i
             for j in range(i +1, n):
                 if nums[j] < nums[min_index]:
                     min_index = j
-            
             nums[i], nums[min_index] = nums[min_index], nums[i]
-        
+            
         return nums
+
+nums = [64, 25, 12, 22, 11]
+obj = Solution()
+print(obj.selectionSort(nums)) #Output: [11, 12, 22, 25, 64]
