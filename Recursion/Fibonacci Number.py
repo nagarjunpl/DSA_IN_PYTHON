a9093156dfca59f7d75580869889e5f0b935d2ac
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-03-17 18:59:15 +0530

    Update Fibonacci Number.py

--- Recursion/Fibonacci Number.py
+++ Recursion/Fibonacci Number.py
@@ -1,15 +1,27 @@
-
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
+        if n <= 1:
+            return n
         
         return self.fib(n - 1) + self.fib(n - 2)
-
 
 a = Solution()
 print(a.fib(2))   # Output: 1
 print(a.fib(3))   # Output: 2
 print(a.fib(6))   # Output: 8
+
+
+
+# Better Approach
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+            
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
+        
+a = Solution()
+print(a.fib(6))   #Output : 8
