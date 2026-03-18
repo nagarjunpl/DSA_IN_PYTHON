f2b66de82d344efaacbf4f0b281bad421adaac6d
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-03-18 20:20:33 +0530

    Update Merge_sort.py

--- Sorting/Merge_sort.py
+++ Sorting/Merge_sort.py
@@ -9,15 +9,17 @@ class Solution:
 
         i = j = 0
         result = []
-    
+
         while i < len(left) and j < len(right):
-            result.append(left[i] if left[i] < right[j] else right[j])
             if left[i] < right[j]:
+                result.append(left[i])
                 i += 1
             else:
+                result.append(right[j])
                 j += 1
-    
+
         return result + left[i:] + right[j:]
-    
+
+nums = [5, 3, 5, 56, 0, 8, 1]
 obj = Solution()
-print(obj.mergeSort([5,3,5,56,0,8,1]))
+print(obj.mergeSort(nums)) #output: [0, 1, 3, 5, 5, 8, 56]
