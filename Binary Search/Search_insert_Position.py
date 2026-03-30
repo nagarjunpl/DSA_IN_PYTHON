7d7f68249c04ed7637979fd7fa4a54b743ee1ef3
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-03-30 18:43:05 +0530

    Refactor searchInsert method for target insertion

--- Binary Search/Search_insert_Position.py
+++ Binary Search/Search_insert_Position.py
@@ -1,20 +1,16 @@
 class Solution:
     def searchInsert(self, nums, target):
-        if target in nums:
-            return nums.index(target)
-            break
-
-        elif nums[0] > target:
+        if nums[0] >= target:
             return 0
-
-        elif nums[-1] < target:
-            return len(nums)
             
         for i in range(len(nums) - 1):
-            if nums[i] < target and nums[i+1] > target:
+            if nums[i] == target:
+                return i
+            if nums[i] < target and nums[i + 1] >= target:
                 return i + 1
 
 nums = [1, 3, 5, 6]
 target = 7
+
 obj = Solution()
-print(obj.searchInsert(nums, target)) # output : 4
+print(obj.searchInsert(nums, target)) # output : 4    
