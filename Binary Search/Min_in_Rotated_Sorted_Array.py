e9b7b350bc8906933fcfc905a84a93ecafe315f2
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-04-06 18:52:02 +0530

    Create Min_in_Rotated_Sorted_Array.py

---
+++ Binary Search/Min_in_Rotated_Sorted_Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMin(self, nums):
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] > nums[r]:
+                l = m + 1
+            else:
+                r = m
+        return nums[l]
+
+nums = [4, 5, 6, 7, -7, 1, 2, 3]
+print(Solution().findMin(nums))
